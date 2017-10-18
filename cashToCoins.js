be7d76ae898ea1428896b1bfeb77dbe25f2708f7
eagobert <eagobert@hotmail.com>
2017-10-18 16:06:49 -0500

    Initial commit

---
+++ cashToCoins.js
@@ -0,0 +1,38 @@
+//Define constant named "dollarAmount"
+const dollarAmount = 158.00;
+
+//Define constant named "piggyBank"
+const piggyBank = {
+    
+        //define keys of object "piggyBank"
+        quarters: ,
+        nickels: ,
+        dimes: ,
+        pennies: 
+    
+    }
+    
+/*
+    //Each change category converted to dollars
+    
+    let convQtrs = 
+    let convNick = 
+    let convDimes = 
+    let convPenn = 
+    
+    //Testing to view if calculation works
+    console.log(convQtrs);
+    console.log(convNick);
+    console.log(convDimes);
+    console.log(convPenn);
+    
+    //Define variable called "dollarAmount" to add the total of all coins
+    let dollarAmount = convQtrs + convNick + convDimes + convPenn;
+    
+    //Log to console the dollar amount
+    console.log(dollarAmount);
+    
+    //Log to console the dollar amount in US dollars with decimal point
+    console.log(dollarAmount.toLocaleString('en-US', { style: 'currency', currency: 'USD' }));
+
+*/
