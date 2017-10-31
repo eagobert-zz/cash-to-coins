a325dd18b139988b458e3ae0f3616ca073bae582
eagobert <eagobert@hotmail.com>
2017-10-30 22:29:29 -0500

    Update changes to piggybank

--- cashToCoins.js
+++ cashToCoins.js
@@ -1,42 +1,27 @@
-//Define constant named "dollarAmount"
-const dollarAmount = 158.00;
-//Each change category converted to dollars
-    
-    let quarters = 
-    let nickels = 
-    let dimes = 
-    let pennies = 
 
-//Define constant named "piggyBank"
+//Define constant named "piggyBank", that accepts the parameter dollarAmount to determine number of quarters, nickels, dimes, and pennies in piggybank
+
 const piggyBank = function(dollarAmount){
-    
+
+    //create formulas to calculate the number of quarters, nickels, dimes, and pennies in the piggybank
+    let quarters = Math.floor(dollarAmount/.25);
+    let dimes = Math.floor((dollarAmount-(quarters*.25))/.10);
+    let nickels = Math.floor((dollarAmount-(quarters*.25)-(dimes*.10))/.05);
+    let pennies = Math.floor((dollarAmount-(quarters*.25)-(dimes*.10)-(nickels*.05))/.01);
+    //still working... pennies is 1 cent off.  need to reconfigure.
+
     return Object.create(null, {
 
         //define keys of object "piggyBank"
-        "Quarters": {value: , enumerable: true},
-        "Nickels": {value: ,enumerable: true},
-        "Dimes": {value: , enumerable: true},
-        "Pennies": {value: , enumerable: true}
+        "Quarters": {value: quarters, enumerable: true},
+        "Dimes": {value: dimes, enumerable: true},
+        "Nickels": {value: nickels,enumerable: true},
+        "Pennies": {value: pennies, enumerable: true}
     })
     
     }
     
-/*
-
-    
-    //Testing to view if calculation works
-    console.log(convQtrs);
-    console.log(convNick);
-    console.log(convDimes);
-    console.log(convPenn);
-    
-    //Define variable called "dollarAmount" to add the total of all coins
-    let dollarAmount = convQtrs + convNick + convDimes + convPenn;
-    
-    //Log to console the dollar amount
-    console.log(dollarAmount);
-    
-    //Log to console the dollar amount in US dollars with decimal point
-    console.log(dollarAmount.toLocaleString('en-US', { style: 'currency', currency: 'USD' }));
-
-*/
+    //Test
+    console.log(piggyBank(158));
+    console.log(piggyBank(1.60))
+    console.log(piggyBank(.24));
